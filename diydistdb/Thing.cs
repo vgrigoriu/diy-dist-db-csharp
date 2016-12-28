69d3b088bc9d16b9dd81d9d5f953d829520daa5a
Victor Grigoriu <vgrigoriu@gmail.com>
2016-12-28 15:54:48 +0200

    Add Thing class

---
+++ diydistdb/Thing.cs
@@ -0,0 +1,18 @@
+﻿namespace diydistdb
+{
+    public class Thing
+    {
+        public int Id { get; }
+
+        public string Value { get; }
+
+        public long Timestamp { get; }
+
+        public Thing(int id, string value, long timestamp)
+        {
+            Id = id;
+            Value = value;
+            Timestamp = timestamp;
+        }
+    }
+}
