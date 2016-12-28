28d0e2c90d2a9cb89ee0fc328c4fe7380b53af95
Victor Grigoriu <vgrigoriu@gmail.com>
2016-12-28 16:46:48 +0200

    Add TODO comments

--- diydistdb/Program.cs
+++ diydistdb/Program.cs
@@ -18,11 +18,13 @@ namespace diydistdb
 
         private static async Task WriteAsync(string[] nodeUrls, Thing thing)
         {
+            // TODO: only works with one node, need to make distributed
             await Node.PutThingAsync(nodeUrls[0], thing);
         }
 
         private static async Task<Thing> ReadAsync(string[] nodeUrls, int id)
         {
+            // TODO: only works with one node, need to make distributed
             return await Node.GetThingAsync(nodeUrls[0], id);
         }
     }
