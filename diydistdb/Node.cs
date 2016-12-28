b57f77e0e2e65d192c320fcd8b2daa0c8d35e15d
Victor Grigoriu <vgrigoriu@gmail.com>
2016-12-28 16:10:40 +0200

    Add Node.PutThing

--- diydistdb/Node.cs
+++ diydistdb/Node.cs
@@ -7,12 +7,19 @@ namespace diydistdb
     {
         private static readonly HttpClient httpClient = new HttpClient();
 
-        public static async Task<Thing> GetThing(string url, int id)
+        public static async Task<Thing> GetThingAsync(string url, int id)
         {
             var endpoint = $"{url}/things/{id}";
             var request = new HttpRequestMessage(HttpMethod.Get, endpoint);
             request.Headers.Accept.ParseAdd("application/json");
             var response = await httpClient.SendAsync(request);
+            return await response.Content.ReadAsAsync<Thing>();
+        }
+
+        public static async Task<Thing> PutThingAsync(string url, Thing thing)
+        {
+            var endpoint = $"{url}/things";
+            var response = await httpClient.PostAsJsonAsync(endpoint, thing);
             return await response.Content.ReadAsAsync<Thing>();
         }
     }
