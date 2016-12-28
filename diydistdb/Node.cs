cbc72a99168c01b2c064accf04d2c85fc02c2b8a
Victor Grigoriu <vgrigoriu@gmail.com>
2016-12-28 16:06:21 +0200

    Add Node.GetThing

--- diydistdb/Node.cs
+++ diydistdb/Node.cs
@@ -1,12 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace diydistdb
 {
-    class Node
+    public static class Node
     {
+        private static readonly HttpClient httpClient = new HttpClient();
+
+        public static async Task<Thing> GetThing(string url, int id)
+        {
+            var endpoint = $"{url}/things/{id}";
+            var request = new HttpRequestMessage(HttpMethod.Get, endpoint);
+            request.Headers.Accept.ParseAdd("application/json");
+            var response = await httpClient.SendAsync(request);
+            return await response.Content.ReadAsAsync<Thing>();
+        }
     }
 }
