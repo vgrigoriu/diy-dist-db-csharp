e51b2987646510af853d38ad839d581188b60be2
Victor Grigoriu <vgrigoriu@gmail.com>
2016-12-28 17:12:34 +0200

    add replication factor and write consistency

--- diydistdb/Program.cs
+++ diydistdb/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace diydistdb
@@ -7,19 +8,44 @@ namespace diydistdb
     {
         static void Main(string[] args)
         {
-            var nodeUrls = new[] { "http://localhost:9999" };
-            WriteAsync(nodeUrls, new Thing(3, "foo")).Wait();
-            WriteAsync(nodeUrls, new Thing(7, "bar")).Wait();
+            var nodeUrls = new[]
+            {
+                "http://localhost:9999",
+                "http://localhost:9998",
+                "http://localhost:9997"
+            };
+            WriteAsync(nodeUrls, 3, 2, new Thing(3, "foo")).Wait();
+            WriteAsync(nodeUrls, 3, 2, new Thing(7, "bar")).Wait();
             var thing3 = ReadAsync(nodeUrls, 3).Result;
             var thing7 = ReadAsync(nodeUrls, 7).Result;
             Console.WriteLine(thing3);
             Console.WriteLine(thing7);
         }
 
-        private static async Task WriteAsync(string[] nodeUrls, Thing thing)
+        private static async Task WriteAsync(
+            string[] nodeUrls,
+            int replicationFactor,
+            int writeConsistency,
+            Thing thing)
         {
-            // TODO: only works with one node, need to make distributed
-            await Node.PutThingAsync(nodeUrls[0], thing);
+            int successCount = 0;
+            foreach (var nodeUrl in nodeUrls.Take(replicationFactor))
+            {
+                try
+                {
+                    await Node.PutThingAsync(nodeUrl, thing);
+                    successCount++;
+                }
+                catch (Exception)
+                {
+                    // ignore
+                }
+            }
+
+            if (successCount < writeConsistency)
+            {
+                throw new Exception($"Only wrote to {successCount} nodes, needed at least {writeConsistency}");
+            }
         }
 
         private static async Task<Thing> ReadAsync(string[] nodeUrls, int id)
